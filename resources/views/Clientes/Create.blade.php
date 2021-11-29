00c30673b03a6f28ad2b8d618de64221a734bf6b
Welbert Sabino <sabino@pcsysdev.com>
2021-11-29 15:06:06 -0300

    projeto exrcusao

---
+++ resources/views/Clientes/Create.blade.php
@@ -0,0 +1,19 @@
+<form>
+  <div class="form-group">
+    <label for="id">Id</label>
+    <input type="text" class="form-control" id="id" name="id" aria-describedby="id" placeholder="" value="">
+  </div>
+  <div class="form-group">
+    <label for="nome">Nome</label>
+    <input type="text" class="form-control" id="nome" name="nome" aria-describedby="nome" placeholder="" value="">
+  </div>
+  <div class="form-group">
+    <label for="nomecracha">Nome Crachá</label>
+    <input type="text" class="form-control" id="nomecracha" name="nomecracha" aria-describedby="nomecracha" placeholder="" value="">
+  </div>
+  <div class="form-group">
+    <label for="endereco">Endereco</label>
+    <input type="text" class="form-control" id="endereco" name="endereco" aria-describedby="endereco" placeholder="" value="">
+  </div>
+  <button type="submit" class="btn btn-primary">Salvar</button>
+</form>
